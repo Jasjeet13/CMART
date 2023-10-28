b93a76a61bdf208cedfa44f1689381f9f1743f11
Kanchan Yadav <122517704+ydv-kanchan@users.noreply.github.com>
2023-10-28 15:56:25 +0530

    Delete FrontPage directory

--- FrontPage/splash.js
+++
@@ -1,7 +0,0 @@
-// Get references to the logo and loading text elements
-const loadingText = document.getElementById('loading-text');
-
-// After 5 seconds, fade out the loading text
-setTimeout(() => {
-    loadingText.style.animation = 'fadeOut 1s ease-in-out';
-}, 5000);
